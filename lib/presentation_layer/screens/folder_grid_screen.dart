d008693e02bf4af131144d67ed8b38c6416e0b2f
johncolani <johnhcolani@gmail.com>
2024-09-09 19:17:57 -0400

    update folder Grid screen

--- lib/presentation_layer/screens/folder_grid_screen.dart
+++ lib/presentation_layer/screens/folder_grid_screen.dart
@@ -260,14 +260,8 @@ class _FolderGridScreenState extends State<FolderGridScreen> {
                   children: [
                     TextField(
                       decoration: const InputDecoration(labelText: 'Folder Name'),
-                      textCapitalization: TextCapitalization.words,
                       onChanged: (value) {
-                        if (value.isNotEmpty){
-                          name = value[0].toUpperCase() + value.substring(1);
-                        }else{
-                          name = value;
-                        }
-
+                        name = value;
                       },
                     ),
                     const SizedBox(height: 10),
